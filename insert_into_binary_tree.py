8f115cb12cfabe1d96238879d1d4118398b78d20
steventejeda <steventejeda@gmail.com>
2023-08-08 23:01:52 -0400

    added iterative solution to insert into binary search tree

--- insert_into_binary_tree.py
+++ insert_into_binary_tree.py
@@ -14,3 +14,21 @@ class Solution:
         else:
             root.left = self.insertIntoBSTRecursively(root.left, val)
         return root
+    
+    def insertIntoBSTIteratively(self, root, val):
+        if not root: 
+            return TreeNode(val)
+
+        cur = root
+
+        while True: 
+            if val > cur.val:
+                if not cur.right: 
+                    cur.right = TreeNode(val)
+                    return root
+                cur = cur.right
+            else:
+                if not cur.left: 
+                    cur.left = TreeNode(val)
+                    return root
+                cur = cur.left
