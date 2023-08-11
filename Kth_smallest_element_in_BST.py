4161f22859285a645b5c3a65d9d94c4b610563ed
steventejeda <steventejeda@gmail.com>
2023-08-11 15:09:03 -0400

    solved problem

---
+++ Kth_smallest_element_in_BST.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def kthSmallest(root: Optional[TreeNode], k: int):
+    n = 0 
+    stack = []
+    cur = root
+
+    while cur or stack:
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+        cur = stack.pop()
+        n += 1
+        if n == k:
+            return cur.val
+        cur = cur.right
