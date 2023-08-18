9ab22f30630063ac4aba7c61d07f2da905ba3b26
steventejeda <steventejeda@gmail.com>
2023-08-18 12:55:19 -0400

    added simple test

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -6,3 +6,9 @@ def containsDuplicate(nums):
             return True
         hashSet.add(n)
     return False
+
+
+print("Does not contain duplicate")
+print(containsDuplicate([8,7,6,2,3,1]))
+print("Contains duplicate")
+print(containsDuplicate([8,7,6,2,3,1,1]))
